d481a6d29ee5206386aca111523b2551b3d7bf2d
Michael Rudy <michael4rudy@gmail.com>
2023-05-09 22:41:15 -0400

    removing key

--- request.py
+++ request.py
@@ -1,18 +1,18 @@
-# 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
-
 import requests
 
 
-
-def request():
+def request(token):
     host = 'https://api.github.com/'
     endpoint = 'users/michaelrudy'
     headers = {"Accept": "application/vnd.github.inertia-preview+json"}
     username = "michaelrudy"
-    token = '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'
+    token = token[0]
     response = requests.get(host+endpoint, headers=headers, auth=(username, token))
     print(response.json())
 
 
 if __name__ == '__main__':
-    request()
+    with open('/Users/michaelrudy/Desktop/Tokens/gh.txt', 'r') as in_file:
+        token = in_file.readlines()
+    
+    request(token)
